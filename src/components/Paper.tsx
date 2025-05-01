105e71883ce13588087fe83a05d272429b8d8eab
seijdeleon <seijdeleon@lbl.gov>
2025-05-01 14:42:38 -0700

    prevent scrollbar from making hard edge on rounded paper

--- src/components/Paper.tsx
+++ src/components/Paper.tsx
@@ -1,4 +1,5 @@
 import { cn } from "@/lib/utils";
+import '@/components/style.css'
 
 export type PaperProps = {
     size?: 'small' | 'medium' | 'large',
@@ -8,21 +9,23 @@ export type PaperProps = {
     children?: React.ReactNode,
 }
 export default function Paper({size="medium", rounded="medium", title, className, children }: PaperProps) {
-    const sizeClass = {
+    const sizeClassMap = {
         small: 'w-32 h-32',
         medium: 'w-64 h-64',
         large: 'w-96 h-96'
     };
-    const roundedEdgeClass = {
+    const roundedEdgeClassMap = {
         none: 'rounded-none',
         small: 'rounded-sm',
         medium: 'rounded-md',
         large: 'rounded-lg'
     }
     return (
-        <article className={cn(`bg-white shadow-lg ${sizeClass[size]} ${roundedEdgeClass[rounded]}`, className)}>
-            {title && <h3 className="w-full text-sky-900 font-medium text-center my-2">{title}</h3>}
-            {children}
+        <article className={cn(`bg-white shadow-lg py-2 px-[1px] ${sizeClassMap[size]} ${roundedEdgeClassMap[rounded]}`, className)}>
+            <div className="h-full w-full overflow-y-auto rounded-scrollbar">
+                {title && <h3 className="w-full text-sky-900 font-medium mb-2 text-center">{title}</h3>}
+                {children}
+            </div>
         </article>
     )
 }
