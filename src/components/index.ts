5e7b370759ef64980f8727094dbca2cbe2412a85
seijdeleon <seijdeleon@lbl.gov>
2025-05-02 12:04:50 -0700

    move npm library imports into parent index file

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,63 +1,63 @@
-import './style.css';
+// import './style.css';
 
 
-export { default as Button } from './Button';
-export type { ButtonProps } from './Button';
+// export { default as Button } from './Button';
+// export type { ButtonProps } from './Button';
 
-export { default as ButtonWithIcon } from './ButtonWithIcon';
-export type { ButtonWithIconProps } from './ButtonWithIcon';
+// export { default as ButtonWithIcon } from './ButtonWithIcon';
+// export type { ButtonWithIconProps } from './ButtonWithIcon';
 
-export { default as ButtonCopyToClipboard } from './ButtonCopyToClipboard';
-export type { ButtonCopyToClipboardProps } from './ButtonCopyToClipboard';
+// export { default as ButtonCopyToClipboard } from './ButtonCopyToClipboard';
+// export type { ButtonCopyToClipboardProps } from './ButtonCopyToClipboard';
 
-export { default as InputSlider } from './InputSlider';
-export type { InputSliderProps } from './InputSlider';
+// export { default as InputSlider } from './InputSlider';
+// export type { InputSliderProps } from './InputSlider';
 
-export { default as Tiled } from './Tiled/Tiled';
-export type { TiledProps } from './Tiled/Tiled';
+// export { default as Tiled } from './Tiled/Tiled';
+// export type { TiledProps } from './Tiled/Tiled';
 
-export { default as HubAppLayout } from './HubAppLayout';
-export type { HubAppLayoutProps } from './HubAppLayout';
+// export { default as HubAppLayout } from './HubAppLayout';
+// export type { HubAppLayoutProps } from './HubAppLayout';
 
-export { default as HubHeader } from './HubHeader';
-export type { HubHeaderProps } from './HubHeader';
+// export { default as HubHeader } from './HubHeader';
+// export type { HubHeaderProps } from './HubHeader';
 
-export { default as HubMainContent } from './HubMainContent';
-export type { HubMainContentProps } from './HubMainContent';
-export { default as HubSidebar } from './HubSidebar';
-export type { HubSidebarProps } from './HubSidebar';
+// export { default as HubMainContent } from './HubMainContent';
+// export type { HubMainContentProps } from './HubMainContent';
+// export { default as HubSidebar } from './HubSidebar';
+// export type { HubSidebarProps } from './HubSidebar';
 
-export { default as ContainerQServer } from './QServer/ContainerQServer';
-export type { ContainerQServerProps } from './QServer/ContainerQServer';
+// export { default as ContainerQServer } from './QServer/ContainerQServer';
+// export type { ContainerQServerProps } from './QServer/ContainerQServer';
 
-export { default as CameraContainer } from './Camera/CameraContainer';
-export type { CameraContainerProps } from './Camera/CameraContainer';
+// export { default as CameraContainer } from './Camera/CameraContainer';
+// export type { CameraContainerProps } from './Camera/CameraContainer';
 
-export { default as CameraCanvas } from './Camera/CameraCanvas';
-export type { CameraCanvasProps } from './Camera/CameraCanvas';
+// export { default as CameraCanvas } from './Camera/CameraCanvas';
+// export type { CameraCanvasProps } from './Camera/CameraCanvas';
 
-export {default as Widget} from './Widget';
-export type {WidgetProps} from './Widget';
+// export {default as Widget} from './Widget';
+// export type {WidgetProps} from './Widget';
 
-export { default as PlotlyHeatmap } from './PlotlyHeatmap';
-export type { PlotlyHeatmapProps } from './PlotlyHeatmap';
+// export { default as PlotlyHeatmap } from './PlotlyHeatmap';
+// export type { PlotlyHeatmapProps } from './PlotlyHeatmap';
 
-export { default as PlotlyScatter } from './PlotlyScatter';
-export type { PlotlyScatterProps } from './PlotlyScatter';
+// export { default as PlotlyScatter } from './PlotlyScatter';
+// export type { PlotlyScatterProps } from './PlotlyScatter';
 
-export { default as DeviceControllerBox } from './DeviceControllerBox';
-export type { DeviceControllerBoxProps } from './DeviceControllerBox';
+// export { default as DeviceControllerBox } from './DeviceControllerBox';
+// export type { DeviceControllerBoxProps } from './DeviceControllerBox';
 
-export { default as Paper } from './Paper';
-export type { PaperProps } from './Paper';
+// export { default as Paper } from './Paper';
+// export type { PaperProps } from './Paper';
 
-export { default as Bento } from './Bento';
-export type { BentoProps } from './Bento';
+// export { default as Bento } from './Bento';
+// export type { BentoProps } from './Bento';
 
 
-//HOOKS
-export { default as useOphydSocket } from '../hooks/useOphydSocket';
+// //HOOKS
+// export { default as useOphydSocket } from '../hooks/useOphydSocket';
 
-//TYPES
-export type { RouteItem } from '../types/navigationRouterTypes';
-export type {Device, Devices } from '../types/deviceControllerTypes';
+// //TYPES
+// export type { RouteItem } from '../types/navigationRouterTypes';
+// export type {Device, Devices } from '../types/deviceControllerTypes';
--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,63 @@
-// import './components/style.css';
+import './components/style.css';
 
-// //COMPONENTS
-// export { default as Button } from './components/Button';
-// export type { ButtonProps } from './components/Button';
+//COMPONENTS
+export { default as Button } from './components/Button';
+export type { ButtonProps } from './components/Button';
 
-// export { default as ButtonWithIcon } from './components/ButtonWithIcon';
-// export type { ButtonWithIconProps } from './components/ButtonWithIcon';
+export { default as ButtonWithIcon } from './components/ButtonWithIcon';
+export type { ButtonWithIconProps } from './components/ButtonWithIcon';
 
-// export { default as ButtonCopyToClipboard } from './components/ButtonCopyToClipboard';
-// export type { ButtonCopyToClipboardProps } from './components/ButtonCopyToClipboard';
+export { default as ButtonCopyToClipboard } from './components/ButtonCopyToClipboard';
+export type { ButtonCopyToClipboardProps } from './components/ButtonCopyToClipboard';
 
-// export { default as InputSlider } from './components/InputSlider';
-// export type { InputSliderProps } from './components/InputSlider';
+export { default as InputSlider } from './components/InputSlider';
+export type { InputSliderProps } from './components/InputSlider';
 
-// export { default as Tiled } from './components/Tiled/Tiled';
-// export type { TiledProps } from './components/Tiled/Tiled';
+export { default as Tiled } from './components/Tiled/Tiled';
+export type { TiledProps } from './components/Tiled/Tiled';
 
-// export { default as HubAppLayout } from './components/HubAppLayout';
-// export type { HubAppLayoutProps } from './components/HubAppLayout';
+export { default as HubAppLayout } from './components/HubAppLayout';
+export type { HubAppLayoutProps } from './components/HubAppLayout';
 
-// export { default as HubHeader } from './components/HubHeader';
-// export type { HubHeaderProps } from './components/HubHeader';
+export { default as HubHeader } from './components/HubHeader';
+export type { HubHeaderProps } from './components/HubHeader';
 
-// export { default as HubMainContent } from './components/HubMainContent';
-// export type { HubMainContentProps } from './components/HubMainContent';
-// export { default as HubSidebar } from './components/HubSidebar';
-// export type { HubSidebarProps } from './components/HubSidebar';
+export { default as HubMainContent } from './components/HubMainContent';
+export type { HubMainContentProps } from './components/HubMainContent';
+export { default as HubSidebar } from './components/HubSidebar';
+export type { HubSidebarProps } from './components/HubSidebar';
 
-// export { default as ContainerQServer } from './components/QServer/ContainerQServer';
-// export type { ContainerQServerProps } from './components/QServer/ContainerQServer';
+export { default as ContainerQServer } from './components/QServer/ContainerQServer';
+export type { ContainerQServerProps } from './components/QServer/ContainerQServer';
 
-// export { default as CameraContainer } from './components/Camera/CameraContainer';
-// export type { CameraContainerProps } from './components/Camera/CameraContainer';
+export { default as CameraContainer } from './components/Camera/CameraContainer';
+export type { CameraContainerProps } from './components/Camera/CameraContainer';
 
-// export { default as CameraCanvas } from './components/Camera/CameraCanvas';
-// export type { CameraCanvasProps } from './components/Camera/CameraCanvas';
+export { default as CameraCanvas } from './components/Camera/CameraCanvas';
+export type { CameraCanvasProps } from './components/Camera/CameraCanvas';
 
-// export {default as Widget} from './components/Widget';
-// export type {WidgetProps} from './components/Widget';
+export {default as Widget} from './components/Widget';
+export type {WidgetProps} from './components/Widget';
 
-// export { default as PlotlyHeatmap } from './components/PlotlyHeatmap';
-// export type { PlotlyHeatmapProps } from './components/PlotlyHeatmap';
+export { default as PlotlyHeatmap } from './components/PlotlyHeatmap';
+export type { PlotlyHeatmapProps } from './components/PlotlyHeatmap';
 
-// export { default as PlotlyScatter } from './components/PlotlyScatter';
-// export type { PlotlyScatterProps } from './components/PlotlyScatter';
+export { default as PlotlyScatter } from './components/PlotlyScatter';
+export type { PlotlyScatterProps } from './components/PlotlyScatter';
 
-// export { default as DeviceControllerBox } from './components/DeviceControllerBox';
-// export type { DeviceControllerBoxProps } from './components/DeviceControllerBox';
+export { default as DeviceControllerBox } from './components/DeviceControllerBox';
+export type { DeviceControllerBoxProps } from './components/DeviceControllerBox';
 
-// export { default as Paper } from './components/Paper';
-// export type { PaperProps } from './components/Paper';
+export { default as Paper } from './components/Paper';
+export type { PaperProps } from './components/Paper';
+
+export { default as Bento } from './components/Bento';
+export type { BentoProps } from './components/Bento';
 
 
-// //HOOKS
-// export { default as useOphydSocket } from './hooks/useOphydSocket';
+//HOOKS
+export { default as useOphydSocket } from './hooks/useOphydSocket';
 
-// //TYPES
-// export type { RouteItem } from './types/navigationRouterTypes';
-// export type {Device, Devices } from './types/deviceControllerTypes';
+//TYPES
+export type { RouteItem } from './types/navigationRouterTypes';
+export type {Device, Devices } from './types/deviceControllerTypes';
