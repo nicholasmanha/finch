84f59fb67668cdc4d749efbf7aaaaf8f1b36d9fa
seijdeleon <seijdeleon@lbl.gov>
2025-05-29 15:10:54 -0700

    add scripts and settings for storybook publish on  gh pages

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -1,7 +1,11 @@
 import type { Preview } from "@storybook/react";
 import {initialize, mswLoader } from "msw-storybook-addon"; // Import the MSW loader
 import '../tailwind.css';
-initialize(); // Initialize MSW
+initialize({
+  serviceWorker: {
+    url: '/finch/mockServiceWorker.js', // required for github pages
+  }
+}); // Initialize MSW
 const preview: Preview = {
   parameters: {
     controls: {
