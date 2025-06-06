26b7007c08aee22553790d1cbc01ed4ffcb350df
Nicholas M <nmanha@lbl.gov>
2025-06-05 20:31:28 -0700

    added DeviceRender component

---
+++ src/components/DeviceRender.tsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { useState } from "react";
+import { Device } from "@/types/deviceControllerTypes";
+import InputNumber from "./InputNumber";
+import Button from "./Button";
+
+export type DeviceRenderProps = {
+    device: Device;
+    
+}
+
+function DeviceRender({ device}: DeviceRenderProps) {
+    if (!device) return;
+    return (
+        <>
+            <div>name: {device.name} value: {device.value}</div><br></br>
+            
+        </>
+
+
+    )
+}
+
+export default DeviceRender
