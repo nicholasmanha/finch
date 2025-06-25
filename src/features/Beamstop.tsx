ed3a4f977cc712ae01912a65e459fcb66f788b06
Seij De Leon <seijdeleon@lbl.gov>
2025-06-25 10:52:06 -0700

    use absolute value comparisons for max beamstop current

--- src/features/Beamstop.tsx
+++ src/features/Beamstop.tsx
@@ -39,8 +39,9 @@ export default function Beamstop(
         const currentDevice = devices[beamstopCurrentName];
         const xDevice = devices[beamstopXName];
         const yDevice = devices[beamstopYName];
+        // The signal at 531 is a negative number, so default to doing absolute value comparisons
         if (currentDevice && currentDevice.value !== null && typeof currentDevice.value === "number") {
-            if (bestCurrent === null || currentDevice.value > bestCurrent) {
+            if (bestCurrent === null || Math.abs(currentDevice.value) > Math.abs(bestCurrent)) {
                 setBestCurrent(currentDevice.value);
                 if (xDevice && xDevice.value !== null && typeof xDevice.value === "number") {
                     setBestXValue(xDevice.value);
@@ -54,11 +55,11 @@ export default function Beamstop(
     return (
         <section className="w-full h-full flex max-h-[800px] max-w-[1200px]">
             <article className="w-1/2 h-full  bg-white flex flex-col p-8">
-                <h3 className="text-4xl text-center">Beamstop Current: {devices[beamstopCurrentName].value} {devices[beamstopCurrentName].units?.slice(0,3)}</h3>
+                <h3 className="text-4xl text-center">Beamstop Current: {devices[beamstopCurrentName] && devices[beamstopCurrentName].value} {devices[beamstopCurrentName] && devices[beamstopCurrentName].units?.slice(0,3)}</h3>
                 <SignalMonitorPlot pv={beamstopCurrentName} className="h-1/2"/>
-                <p>Best Beamstop Current Value: {bestCurrent} {devices[beamstopCurrentName].units}</p>
-                <p>Best Beamstop X value: {bestXValue} {devices[beamstopXName].units}</p>
-                <p>Best Beamstop Y value: {bestYValue} {devices[beamstopYName].units}</p>
+                <p>Best Beamstop Current Value: {bestCurrent} {devices[beamstopCurrentName] && devices[beamstopCurrentName].units}</p>
+                <p>Best Beamstop X value: {bestXValue} {devices[beamstopXName] && devices[beamstopXName].units}</p>
+                <p>Best Beamstop Y value: {bestYValue} {devices[beamstopYName] && devices[beamstopYName].units}</p>
                 <div className="flex justify-center items-center py-8">
                     <Button cb={goToBest} text="Go To Best"/>   
                 </div>
