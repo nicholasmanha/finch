d107cac0982231c8378a8671421a462edf8a57ce
Nicholas Manha <nmanha@lbl.gov>
2025-07-04 19:38:51 -0700

    fixed tab styling

--- src/components/Tabs/Tab.tsx
+++ src/components/Tabs/Tab.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { useTabsContext } from "./context/TabsContext";
 import { TabProps } from "./types/tabs";
+import { XCircle } from "@phosphor-icons/react";
 
 export const Tab: React.FC<TabProps> = ({
   value,
@@ -13,26 +14,33 @@ export const Tab: React.FC<TabProps> = ({
 
   return (
     <>
-      <button
-        className={`px-4 py-2 text-xl transition-colors duration-200 border-b-2 ${
+      <div
+        className={`flex justify-center px-4 py-2 text-xl transition-colors duration-200 border-b-2 ${
           isActive
-            ? "text-blue-400 border-blue-600"
+            ? "text-blue-400 border-blue-600 bg-white/10"
             : "text-white border-transparent hover:text-gray-400 hover:border-gray-300"
         } ${className}`}
-        onClick={() => setActiveTab(value)}
-        role="tab"
-        aria-selected={isActive}
-        aria-controls={`panel-${value}`}
-        id={`tab-${value}`}
       >
-        {children}
-      </button>
-      <button
-        onClick={() => removeTab(value)}
-        className="ml-2 text-red-500 hover:text-red-700 text-4xl"
-      >
-        x
-      </button>
+        <div className="flex flex-col justify-center">
+          <button
+            onClick={() => setActiveTab(value)}
+            role="tab"
+            aria-selected={isActive}
+            aria-controls={`panel-${value}`}
+            id={`tab-${value}`}
+          >
+            {children}
+          </button>
+        </div>
+        <div className="flex flex-col justify-center align-center">
+          <button
+            onClick={() => removeTab(value)}
+            className="ml-2 text-white hover:text-red-500 transition-colors duration-200 text-4xl"
+          >
+            <XCircle size={24} />
+          </button>
+        </div>
+      </div>
     </>
   );
 };
