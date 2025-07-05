03d2f2f5d591622fe4ac7f2a9eb640879d32cda9
Nicholas Manha <nmanha@lbl.gov>
2025-07-04 19:43:57 -0700

    fixed tab hitbox

--- src/components/Tabs/Tab.tsx
+++ src/components/Tabs/Tab.tsx
@@ -14,33 +14,34 @@ export const Tab: React.FC<TabProps> = ({
 
   return (
     <>
-      <div
-        className={`flex justify-center px-4 py-2 text-xl transition-colors duration-200 border-b-2 ${
-          isActive
-            ? "text-blue-400 border-blue-600 bg-white/10"
-            : "text-white border-transparent hover:text-gray-400 hover:border-gray-300"
-        } ${className}`}
+      <button
+        onClick={() => setActiveTab(value)}
+        role="tab"
+        aria-selected={isActive}
+        aria-controls={`panel-${value}`}
+        id={`tab-${value}`}
       >
-        <div className="flex flex-col justify-center">
-          <button
-            onClick={() => setActiveTab(value)}
-            role="tab"
-            aria-selected={isActive}
-            aria-controls={`panel-${value}`}
-            id={`tab-${value}`}
-          >
-            {children}
-          </button>
+        <div
+          className={`flex justify-center px-4 py-2 text-xl transition-colors duration-200 border-b-2 ${
+            isActive
+              ? "text-blue-400 border-blue-600 bg-white/10"
+              : "text-white border-transparent hover:text-gray-400 hover:border-gray-300"
+          } ${className}`}
+          onClick={() => setActiveTab(value)}
+        >
+          <div className="flex flex-col justify-center">
+            <div>{children}</div>
+          </div>
+          <div className="flex flex-col justify-center align-center">
+            <button
+              onClick={() => removeTab(value)}
+              className="ml-2 text-white hover:text-red-500 transition-colors duration-200 text-4xl"
+            >
+              <XCircle size={24} />
+            </button>
+          </div>
         </div>
-        <div className="flex flex-col justify-center align-center">
-          <button
-            onClick={() => removeTab(value)}
-            className="ml-2 text-white hover:text-red-500 transition-colors duration-200 text-4xl"
-          >
-            <XCircle size={24} />
-          </button>
-        </div>
-      </div>
+      </button>
     </>
   );
 };
