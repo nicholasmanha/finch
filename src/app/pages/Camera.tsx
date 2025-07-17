57947f7cff6df1d06c077936e2665376a7592821
Seij De Leon <seijdeleon@lbl.gov>
2025-07-17 12:04:18 -0700

    screen size styling

--- src/app/pages/Camera.tsx
+++ src/app/pages/Camera.tsx
@@ -1,7 +1,7 @@
 import CameraContainer from "@/components/Camera/CameraContainer";
 export default function Camera() {
     return (
-        <div className="flex flex-col items-center justify-center h-screen">
+        <div className="flex flex-col items-center justify-center h-full">
             <h1 className="text-3xl font-bold mb-4">Area Detector Live Stream</h1>
             <CameraContainer prefix="Basler5472" enableControlPanel={true} enableSettings={false} canvasSize="medium"/>
         </div>
